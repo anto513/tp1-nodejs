67e4a3bc81b923911d080886124ca018c867bd8d
anto <antonela.testa@mjus.gba.gob.ar>
2024-05-25 14:33:26 -0300

    index

--- js/validaciones.js
+++ js/validaciones.js
@@ -8,24 +8,14 @@ form.addEventListener('submit', (event) => {
 
   // Validar los campos obligatorios
   const nameInput = document.getElementById('name');
-  const lastNameInput = document.getElementById('lastName');
   const emailInput = document.getElementById('email');
-  const genderRadios = document.querySelectorAll('input[name="gender"]');
-  const nationalitySelect = document.getElementById('nationality');
-  const imageInput = document.getElementById('image-input');
+  const provinciaSelect = document.getElementById('provincia');
 
   if (nameInput.value.trim() === '') {
     isValid = false;
     nameInput.classList.add('invalid');
   } else {
     nameInput.classList.remove('invalid');
-  }
-
-  if (lastNameInput.value.trim() === '') {
-    isValid = false;
-    lastNameInput.classList.add('invalid');
-  } else {
-    lastNameInput.classList.remove('invalid');
   }
 
   if (emailInput.value.trim() === '' || !isValidEmail(emailInput.value)) {
@@ -35,40 +25,13 @@ form.addEventListener('submit', (event) => {
     emailInput.classList.remove('invalid');
   }
 
-  let genderSelected = false;
-  genderRadios.forEach((radio) => {
-    if (radio.checked) {
-      genderSelected = true;
-    }
-  });
-  if (!genderSelected) {
+  if (provinciaSelect.value === '') {
     isValid = false;
-    document.querySelectorAll('.radio-group label').forEach((label) => {
-      label.classList.add('invalid');
-    });
+    provinciaSelect.classList.add('invalid');
   } else {
-    document.querySelectorAll('.radio-group label').forEach((label) => {
-      label.classList.remove('invalid');
-    });
+    provinciaSelect.classList.remove('invalid');
   }
 
-  if (nationalitySelect.value === '') {
-    isValid = false;
-    nationalitySelect.classList.add('invalid');
-  } else {
-    nationalitySelect.classList.remove('invalid');
-  }
-
-  if (imageInput.value === '') {
-    isValid = false;
-    document.querySelector('.image-upload label').classList.add('invalid');
-  } else {
-    document.querySelector('.image-upload label').classList.remove('invalid');
-  }
-
-  if (isValid) {
-    form.submit();
-  }
 });
 
 function isValidEmail(email) {
